81e512bf54b09ebfd96a06a725f7c53594c96c9f
Veiasai <917971502@qq.com>
2018-03-31 10:36:50 +0800

    add log

--- src/main/java/com/veiasai/Service.java
+++ src/main/java/com/veiasai/Service.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Scanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 @EnableAutoConfiguration
 public class Service {
+    protected final Logger logger=LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/")
     @ResponseBody
     String home() {
@@ -19,16 +24,19 @@ public class Service {
     @RequestMapping("/calculator/{expression}")
     @ResponseBody
     public String Cal(@PathVariable String expression){
+        logger.debug("访问cal:" + expression);
         Calculator cal = new Calculator();
         Scanner in = new Scanner(expression);
         cal.setInput(new Scanner(in.nextLine()));
         cal.cal();
         if (cal.getState() == 0)
         {
+            logger.debug("success:" + cal.getResult_double());
             return String.valueOf(cal.getResult_double());
         }
         else
         {
+            logger.debug("fail:" + cal.getE().toString());
             return cal.getE().toString();
         }
     }
